3699f2e975f82ff2ea26aeb82bd810ea28f9c19d
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-07-28 10:27:49 +0800

    project initialization

---
+++ SplitView/SplitView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SplitView
+//
+//  Created by LLDM on 28/07/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SplitView/SplitView/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  SplitView
+//
+//  Created by LLDM on 28/07/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
